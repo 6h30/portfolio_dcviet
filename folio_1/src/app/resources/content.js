9028711cb1d3f815314555e05ab70f318229bee7
6h30 <truongduc1108@gmail.com>
2025-03-05 12:44:58 +0000

    update foli0_vn

--- folio_1/src/app/resources/content.js
+++ folio_1/src/app/resources/content.js
@@ -6,7 +6,7 @@ const person = {
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Developer",
+  role: "Lập trình viên",
   avatar: "/images/avatar.png",
   location: "Asia/Ho_Chi_Minh", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English B1", "VN L1"], // optional: Leave the array empty if you don't want to display languages
@@ -14,11 +14,10 @@ const person = {
 
 const newsletter = {
   display: true,
-  title: <>Subscribe to {person.firstName}'s Newsletter</>,
+  title: <>Đăng kí nhận bản tin từ {person.firstName}</>,
   description: (
     <>
-      I occasionally write about design, technology, and share thoughts on the intersection of
-      creativity and engineering.
+      Thỉnh thoảng tôi viết về thiết kế, công nghệ và chia sẻ suy nghĩ về mối quan hệ giữa sáng tạo và kỹ thuật.
     </>
   ),
 };
@@ -60,14 +59,14 @@ const home = {
   headline: <>Fullstack developer</>,
   subline: (
     <>
-      I'm Dcviet, a freelance developer in <InlineCode>VietNam</InlineCode>.
+      Tui là 1 <InlineCode>lập trình viên</InlineCode> làm việc tự do.
     </>
   ),
 };
 
 const about = {
-  label: "About",
-  title: "About me",
+  label: "dcviet",
+  title: "Về tôi",
   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
   tableOfContent: {
     display: true,
@@ -82,16 +81,16 @@ const about = {
   },
   intro: {
     display: true,
-    title: "Introduction",
+    title: "Giới thiệu",
     description: (
       <>
-        I am a developer with a passion for transforming complex challenges into simple, elegant design solutions. My work encompasses digital interfaces, interactive experiences, and the intersection of design and technology.
+        Tôi có niềm đam mê biến những thách thức phức tạp thành các giải pháp thiết kế đơn giản, thanh lịch. Công việc của tôi bao gồm: thiết kế giao diện, trải nghiệm tương tác và tìm kiếm sự giao thoa giữa thiết kế và công nghệ.
       </>
     ),
   },
   work: {
     display: true, // set to false to hide this section
-    title: "Work Experience",
+    title: "Kinh nghiệm làm việc",
     experiences: [
       {
         company: "duc& Agency",
@@ -99,10 +98,10 @@ const about = {
         role: "Freelance Developer",
         achievements: [
           <>
-            Redesigned the UI/UX for the website, resulting in a 20% increase in user engagement and 30% faster load times.
+            Thiết kế lại UI/UX cho trang web, giúp tăng 20% ​​mức độ tương tác của người dùng và thời gian tải nhanh hơn 30%.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.
+            Tiên phong trong việc tích hợp các công cụ AI vào quy trình thiết kế, cho phép các bạn thiết kế lặp lại nhanh hơn 50%.
           </>,
         ],
         images: [
@@ -139,11 +138,10 @@ const about = {
         role: "Designer and Developer",
         achievements: [
           <>
-            Optimized components, data processing the nextjs platform, improving seo by 40%.
+            Tối ưu hóa các thành phần, xử lý dữ liệu nền tảng nextjs, cải thiện seo lên 40%.
           </>,
           <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
+            Dẫn dắt một nhóm chức năng chéo (kiến trúc sư, đối tác thi công, thiết kế sản phẩm) để ra mắt dòng sản phẩm mới, góp phần tăng 15% tổng doanh thu của công ty.
           </>,
         ],
         images: [
@@ -165,25 +163,25 @@ const about = {
   },
   studies: {
     display: true, // set to false to hide this section
-    title: "Studies",
+    title: "Học vấn",
     institutions: [
       {
-        name: "University of Science - VNUHCM",
-        description: <>Studied infomation technology.</>,
+        name: "Đại học Khoa Học Tự Nhiên TpHCM",
+        description: <>Ngành Công Nghệ Thông Tin.</>,
       },
       {
-        name: "Build the Future",
-        description: <>Studied elearning platform and personal branding.</>,
+        name: "Định hướng phát triển",
+        description: <>Nghiên cứu nền tảng học trực tuyến và phát triển thương hiệu cá nhân.</>,
       },
     ],
   },
   technical: {
     display: true, // set to false to hide this section
-    title: "Technical skills",
+    title: "Kỹ năng kỹ thuật",
     skills: [
       {
-        title: "UI Implementation",
-        description: <>Translating Figma designs into responsive, high-fidelity user interfaces using React, Next.js, Vue, or Angular, delivering optimized and scalable front-end solutions.
+        title: "Triển khai UI",
+        description: <>Chuyển đổi các thiết kế Figma thành giao diện người dùng có độ phản hồi cao, độ trung thực cao bằng React, Next.js, Vue hoặc Angular, cung cấp các giải pháp front-end được tối ưu hóa và có thể mở rộng.
         </>,
         // optional: leave the array empty if you don't want to display images
         images: [
@@ -202,12 +200,12 @@ const about = {
         ],
       },
       {
-        title: "Design-to-Code Workflow",
-        description: <>Experienced in streamlining collaboration between design and development teams, ensuring seamless integration of UI designs with maintainable codebases.</>,
+        title: "Quy trình làm việc từ thiết kế đến mã",
+        description: <>Có kinh nghiệm trong việc hợp lý hóa sự hợp tác giữa các nhóm thiết kế và phát triển, đảm bảo tích hợp liền mạch các thiết kế UI với các cơ sở mã có thể bảo trì.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
           {
-            src: "/images/projects/project-01/cover-04a.png",
+            src: "/images/projects/project-01/cover-04d.png",
             alt: "Project image",
             width: 16,
             height: 9,
@@ -215,8 +213,8 @@ const about = {
         ],
       },
       {
-        title: "Additional Tools",
-        description: <>Familiar with state management (Redux, Vuex), API integration (REST, GraphQL), and performance optimization techniques for modern web applications.</>,
+        title: "Các công cụ bổ sung",
+        description: <>Quen thuộc với quản lý trạng thái (Redux, Vuex), tích hợp API (REST, GraphQL) và các kỹ thuật tối ưu hóa hiệu suất cho các ứng dụng web hiện đại.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
           {
@@ -233,24 +231,24 @@ const about = {
 
 const blog = {
   label: "Blog",
-  title: "Writing about design and tech...",
-  description: `Read what ${person.name} has been up to recently`,
+  title: "Viết về thiết kế và công nghệ...",
+  description: `Đọc những gì ${person.name} đã viết gần đây`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
 };
 
 const work = {
-  label: "Work",
-  title: "My projects",
+  label: "Dự án",
+  title: "Dự án đã thực hiện",
   description: `Designed and developed by ${person.name}`,
   // Create new project pages by adding a new .mdx file to app/blog/posts
   // All projects will be listed on the /home and /work routes
 };
 
 const gallery = {
-  label: "Gallery",
-  title: "My photo gallery",
-  description: `A photo collection by ${person.name}`,
+  label: "Ảnh dự án",
+  title: "Bộ sưu tập ảnh công việc",
+  description: `Một bộ sưu tập ảnh của ${person.name}`,
   // Images from https://pexels.com
   images: [
     {
